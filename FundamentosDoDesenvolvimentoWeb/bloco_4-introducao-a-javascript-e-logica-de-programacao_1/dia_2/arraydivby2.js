1ce509650545c4dd705c27e123d05a024745e995
Felipe Flores <felipefloresvendas@gmail.com>
2021-02-25 20:53:18 -0300

    realizado exercicio 8

---
+++ FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_2/arraydivby2.js
@@ -0,0 +1,7 @@
+let arrayExample = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  arrayExample.push(index);
+}
+
+for (let index2 = 1;)
---
+++ FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_2/ifNumLessThan.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultadoSoma = 0
+
+for(let index = 0; index < numbers.length; index += 1) {
+  let numeroAtual = numbers[index];
+   resultadoSoma = resultadoSoma + numeroAtual;
+}
+
+arithmeticAverage = resultadoSoma / numbers.length;
+
+if (arithmeticAverage > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}

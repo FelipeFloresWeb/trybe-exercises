8be69aadf98bc1984179cab722862cbf45d14198
Felipe Flores <felipefloresvendas@gmail.com>
2021-02-28 11:05:46 -0300

    realizado 2 parte exercicio 4

--- FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_3/asteriskspyramid.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_3/asteriskspyramid.js
@@ -1,31 +1,37 @@
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+// n = 5
+
+//   *
+//  ***
+// *****
+
 let n = 5;
 let symbol = '*';
 let imputLine = '';
-let spaceCount = 0;
 let canImput = Math.round(n / 2);
-
 
 for (let index = 1; index <= n; index += 1) {
   
-  for (let index2 = 0; index2 < 5; index2 += 1) {
-    if(index >= canImput) {
+  for (let index2 = 1; index2 <= n; index2 += 1) {
+    
+    if(index2 == canImput) {
       imputLine += symbol;
-    } else {
-      imputLine = imputLine + ' ';
+      canImput -= 2;
+      symbol += '**';
+      if(canImput < 1) {
+        canImput == 1;
+      }
+    } else if(index2 != canImput) {
+      imputLine += ' ';
     }
     
   }
 
-  console.log(imputLine);
-  imputLine = '';
-  symbol += '**';
-
+  if (index % 2 != 0) {
+    console.log(imputLine);
+    imputLine = '';
+  } else {
+    imputLine = '';
+  }
+  
 }
-
-// for (let index3 = 1; index3 < n; index23 += 1) {
-//   if (index3 < canImput) {
-//     imputLine = imputLine + ' ';
-//     } else {
-//       imputLine = imputLine + symbol;
-//     }
-//   }

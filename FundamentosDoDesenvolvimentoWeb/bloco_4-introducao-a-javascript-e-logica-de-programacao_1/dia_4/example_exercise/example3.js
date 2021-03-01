b51d47e5d43de8c06109b7b0e18420863a5e56bd
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-01 18:46:11 -0300

    realizado terceiro exercicio bonus

--- FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_4/example_exercise/example3.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_4/example_exercise/example3.js
@@ -44,8 +44,97 @@ function posNegZero(a) {
   if(a > 0) {
     return 'positive';
   } else if (a < 0) {
-    return 'negative'
+    return 'negative';
   } else {
-    return 'zero'
+    return 'zero';
   }
+}
+
+function isTriangle(a, b, c) {
+
+  let aTriangle = a + b + c;
+
+  if(aTriangle === 180) {
+    return 'Is triangle';
+  } else if (a < 0 || b < 0|| c < 0) {
+    return 'Enter a valid number';
+  } else {
+    return 'Is not Triangle';
+  }
+}
+
+function movPieceChess (text) {
+
+  let text = 'BISHOP';
+
+  result = text.toLowerCase()
+  
+  if (result == 'bishop') {
+    console.log('diagonal');
+  } else if (result == 'horse') {
+    console.log('"L" (can jump over the pieces)');
+  } else if (result == 'tower') {
+    console.log('horizontal and vertical');
+  } else if (result == 'queen') {
+    console.log('diagonal, horizontal and vertical');
+  } else if (result == 'king') {
+    console.log('diagonal and horizontal(1 square)');
+  } else if (result == 'pawn') {
+    console.log('horizontal(1 square) diagonal for kill');
+  } else {
+    console.log('This is not a valid piece')
+  }
+  
+}
+
+function whatIsMyNote(Porcentagem) {
+  let Porcentagem = 52;
+
+if (Porcentagem > 100 || Porcentagem < 0) {
+  Porcentagem = Porcentagem;
+} else if (Porcentagem >= 90) {
+  Porcentagem = 'A';
+} else if (Porcentagem >= 80) {
+  Porcentagem = 'B';
+} else if (Porcentagem >= 70) {
+  Porcentagem = 'C';
+} else if (Porcentagem >= 60) {
+  Porcentagem = 'D';
+} else if (Porcentagem >= 50) {
+  Porcentagem = 'E';
+} else if (Porcentagem < 50) {
+  Porcentagem = 'F';
+}
+
+switch (Porcentagem) {
+  case 'A':
+    console.log(Porcentagem);
+    break;
+
+  case 'B':
+    console.log(Porcentagem);
+    break;
+
+  case 'C':
+    console.log(Porcentagem);
+    break;
+
+  case 'D':
+    console.log(Porcentagem);
+    break;
+
+  case 'E':
+    console.log(Porcentagem);
+    break;
+
+  case 'F':
+    console.log(Porcentagem);
+    break;
+
+  default:
+    console.log('Error');
+}
+
+return 'Sua Nota final é ' + Porcentagem;
+
 }

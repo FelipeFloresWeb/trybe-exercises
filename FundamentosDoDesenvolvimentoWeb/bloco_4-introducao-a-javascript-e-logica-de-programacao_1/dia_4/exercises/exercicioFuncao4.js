f236519896f440c2adcc0bf9b1f6ee9c55df23ef
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-02 13:03:41 -0300

    realizado exercicio4

--- FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_4/exercises/exercicioFuncao4.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_4/exercises/exercicioFuncao4.js
@@ -7,14 +7,30 @@ let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 function biggestName(text) {
 
   text = [];
+  let biggerNameCrescentOrder = [];
+  let theBiggerName = '';
 
-  for(let index in text) {
+  for(let index in array) {
 
     text.push(array[index]);
 
   }
 
-  return text[index];
+  for(let index = 0;index < text.length; index += 1) {
+
+    if (text[index] == text.length - 1) {
+      text[index] = text[index];
+    } else if(text[index] > text[index+1]) {
+      biggerNameCrescentOrder.push(text[index]);
+    } else {
+      text[index] = text[index];
+    }
+
+  }
+
+  theBiggerName += biggerNameCrescentOrder[biggerNameCrescentOrder.length - 1];
+
+  return theBiggerName;
 
 }
 

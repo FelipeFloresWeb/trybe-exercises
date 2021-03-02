fd6fe66a1fe2dd7b983b1813406439d5175c9281
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-01 23:17:12 -0300

    Finalizado primeira parte dos exercicios

--- FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_4/exercises/exercicio5.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_4-introducao-a-javascript-e-logica-de-programacao_1/dia_4/exercises/exercicio5.js
@@ -12,15 +12,27 @@ let info = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
+info.recorrente = 'Sim';
+
 let info2 = {
-  personagem2: "Tio Patinhas",
-  origem2: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota2: "O último MacPatinhas",
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+};
+
+info2.recorrente = 'Sim';
+
+if (info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+  info.recorrente = 'Ambos '
+  info2.recorrente = 'recorrentes'
 }
 
-info.recorrente = 'Sim';
-info2.recorrente2 = 'Sim';
+if (info2.personagem === 'Tio Patinhas') {
+  info2.personagem = " e Tio Patinhas";
+  info2.origem = " e Christmas on Bear Mountain, Dell's Four Color Comics #178";
+  info2.nota = "e O último MacPatinhas";
+}
 
-for(let index in info in info2) {
-  console.log(info[index] + ' e ' + info2[index]);
+for(let index in info) {
+  console.log(info[index] + info2[index]);
 }

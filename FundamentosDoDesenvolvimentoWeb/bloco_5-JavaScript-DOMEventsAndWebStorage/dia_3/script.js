92414ceb8f48ca76753cb4cb162fd6cf1506229c
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-07 16:07:56 -0300

    exercise 10 Done

--- FundamentosDoDesenvolvimentoWeb/bloco_5-JavaScript-DOMEventsAndWebStorage/dia_3/script.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_5-JavaScript-DOMEventsAndWebStorage/dia_3/script.js
@@ -23,7 +23,8 @@ createDaysOfTheWeek();
 // Copiar
 // const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 function liCreator() {
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   let daysList = document.querySelector("#days");
   for(let index = 0; index < dezDaysList.length; index += 1) {
     let daysListItem = document.createElement("li")
@@ -187,13 +188,28 @@ function taskSelect() {
       divTask.className = "task";
     } else {
       divTask.className = "task-selected";
-    }    
+    }
   }
 }
 taskSelect();
 // Exercício 10:
 // Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
 // Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+
+function changeDayColor() {
+  const days = document.querySelector('#days');
+  const divTask = document.querySelector('.task');
+
+  function colorDay(event) {
+    if (divTask.className === 'task') {
+      event.target.style.color = 'rgb(119,119,119)';
+    } else {
+      event.target.style.color = 'green';
+    }
+  }
+  days.addEventListener('click', colorDay);
+}
+changeDayColor();
 // Bônus:
 // Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
 // Se nenhum caractere for inserido no campo input , a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".

b0aa67bfada4acfeb48a9a7c3d626e306065955e
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-09 13:09:58 -0300

    adicionado editor de texto niceEditor

--- FundamentosDoDesenvolvimentoWeb/bloco_5-JavaScript-DOMEventsAndWebStorage/dia_4/script.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_5-JavaScript-DOMEventsAndWebStorage/dia_4/script.js
@@ -1,31 +1,37 @@
+let divText = document.querySelector("#text");
+let reloadPage = localStorage.getItem('boxText');
+let elementP = document.createElement("p");
+elementP.innerHTML = reloadPage;
+divText.appendChild(elementP);
 function addInputText() {
   let divText = document.querySelector("#text");
-  let boxText = document.querySelector("#inpuText");
   let buttonAdd = document.querySelector("#addButton");
   let clearButton = document.querySelector("#clearButton");
-
   buttonAdd.addEventListener("click", addTextButton);
   clearButton.addEventListener("click", removeTextButton);
 
   function addTextButton() {
-    let newText = '';
-    let newP = document.createElement("p");
+    let boxText = document.querySelectorAll("div")[90];// filho
+    let createElementP = document.createElement("p");
     let errorMensagem = 'Nenhum conteúdo foi digitado!';
     let thanksMensagem = 'Seu conteúdo foi adicionado em nossa página! Agradecemos pela contribuição';
-    if (boxText.value === '') {
+    if (boxText.innerHTML === '') {
       alert(errorMensagem);
     } else {
-      newP.className = 'newText';
-      newP.innerHTML = boxText.value;
-      divText.appendChild(newP);
+      createElementP.className = 'newText';
+      createElementP.innerHTML = boxText.innerHTML;
+      divText.appendChild(createElementP);
       alert(thanksMensagem);
-      localStorage.setItem('newP', 'innerHTML');
+      let savedStorage = localStorage.getItem('boxText');
+      savedStorage += boxText.innerHTML;
+      localStorage.setItem('boxText', savedStorage);
     }
   }
 
   function removeTextButton(event) {
+    let boxText = document.querySelectorAll("div")[90];// filho
+    boxText.innerHTML = '';
     let message = 'A caixa de texto foi redefinida!'
-    boxText.value = '';
     alert(message);
   }
 }

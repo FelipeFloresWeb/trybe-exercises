9e0146d006ba8d7743359ce00f6ed7338f708ac2
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-18 15:45:38 -0300

    Corrigido Eslint, melhorado codigo exercicio 6.2

--- FundamentosDoDesenvolvimentoWeb/bloco_6-HTML-e-CSS-Forms-Flexbox-e-Flexbox-Responsivo/dia_2/script.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_6-HTML-e-CSS-Forms-Flexbox-e-Flexbox-Responsivo/dia_2/script.js
@@ -1,10 +1,7 @@
 const selectElementEstado = document.querySelector('#estado');
 const selectDataInicio = document.querySelector('#datainicio');
 const selectButton = document.querySelector('#send');
-const selectButtonClear = document.querySelector('#clear');
 const selectBody = document.querySelector('body');
-const selectForm = document.querySelector('#formulario');
-const selectName = document.querySelector('#name');
 
 function createState() {
   const estados = ['Acre', 'Amapá', 'Amazonas', 'Pará', 'Rondônia', 'Roraima', 'Tocantins', 'Alagoas', 'Bahia', 'Ceará', 'Maranhão', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio Grande do Norte', 'Sergipe', 'Goiás', 'Mato Grosso', 'Mato Grosso do Sul', 'Distrito Federal', 'Espírito Santo', 'Minas Gerais', 'São Paulo', 'Rio de Janeiro', 'Paraná', 'Rio Grande do Sul', 'Santa Catarina'];
@@ -18,23 +15,23 @@ function createState() {
 }
 window.onload = createState();
 
-function checkDateInput(data) {
-  data = selectDataInicio.value.split('/')
+function checkDateInput() {
+  const dataSplited = selectDataInicio.value.split('/');
   const errorMsg = 'Formato de data inválido!';
+  const alert = errorMsg;
 
-  if (parseInt(data[0]) > 31 || parseInt(data[0]) < 1) {
+  if (parseInt(dataSplited[0], 10) > 31 || parseInt(dataSplited[0], 10) < 1) {
     alert(errorMsg);
-  } if (parseInt(data[1]) > 12 || parseInt(data[1]) < 1) {
+  } if (parseInt(dataSplited[1], 10) > 12 || parseInt(dataSplited[1], 10) < 1) {
     alert(errorMsg);
-  } if (parseInt(data[2]) > 2021 || parseInt(data[2]) < 1) {
+  } if (parseInt(dataSplited[2], 10) > 2021 || parseInt(dataSplited[2], 10) < 1) {
     alert(errorMsg);
   }
-
 }
 
 function sendInformations() {
   const selectInputs = document.querySelectorAll('input');
-  let selectTextArea = document.querySelector('#curriculo');
+  const selectTextArea = document.querySelector('#curriculo');
   const createDiv = document.createElement('div');
 
   createDiv.id = 'divformulario';
@@ -50,23 +47,22 @@ function sendInformations() {
   const createP = document.createElement('p');
   createP.innerHTML = selectTextArea.value;
   createDiv.appendChild(createP);
-  
 }
 
 function preventDefaultSettings(event) {
   event.preventDefault();
-  const allInputs = document.querySelectorAll('input');
-  var msg = '';
+  const allInputs = document.querySelectorAll('input', 'select', 'textarea');
+  let msg = '';
+  const alert = msg;
   for (let index = 0; index < allInputs.length; index += 1) {
     const element = allInputs[index];
     if (element[index].value === '') {
-      msg += 'Campo' + element[index].id + 'está vazio<br>';
+      msg += `Campo ${element[index].id} está vazio<br>`;
     } if (element[index].type === 'radio' && !element[index].checked) {
-      msg += 'Campo' + element[index].id + 'não esta selecionado<br>';
+      msg += `Campo ${element[index].id} não esta selecionado<br>`;
     }
-    
   }
-  alert(msg);
+  alert();
 }
 
 selectButton.addEventListener('click', preventDefaultSettings);

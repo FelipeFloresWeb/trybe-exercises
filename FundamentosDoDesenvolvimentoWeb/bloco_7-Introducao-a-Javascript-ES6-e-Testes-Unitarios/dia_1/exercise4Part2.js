c5f17985825985fa33255ff620c260607252f51f
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-24 13:57:03 -0300

    Exercise 2 function 2 Done

--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_1/exercise4Part2.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_1/exercise4Part2.js
@@ -11,16 +11,19 @@
 
 // consulta método .replace() https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replaceola%20tenho%2032%20anos%20e%20faz%202%20dias%20let%20numbers%20=%20frase(//w(x)+/g)
 
-const skills = ['Javascipt', ' HTML', ' CSS', ' Git', ' GitHub'];
+const skills = [' Javascipt', ' HTML', ' CSS', ' Git', ' GitHub'];
 
-function addTrybexWin(string) {
+const addTrybexAqui = (string) => {
   // let re = /x/gi;
   const output = 'Tryber x aqui!';
   const result = output.replace('x', string);
   return result;
-}
+};
 
-console.log(addTrybexWin(skills));
+// eslint-disable-next-line no-console
+console.log(addTrybexAqui(skills));
+
+// Segunda Opção:
 
 // function addTrybexAqui(string) {
 //   let output = 'Tryber x aqui!';
@@ -44,3 +47,15 @@ console.log(addTrybexWin(skills));
 // Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
 // JavaScript;
 // HTML; ... #goTrybe".
+
+// const phrase = addTrybexWin(skills);
+const phrase2 = () => {
+  const phrase = addTrybexAqui(skills);
+  const orderArray = skills.sort();
+  const newphrase = 'Minhas cinco principais habilidades são:';
+
+  return `${phrase} ${newphrase} ${orderArray} #goTrybe`;
+};
+
+// eslint-disable-next-line no-console
+console.log(phrase2());

5e3acf232d1ec3a6dee5cd308e86bce436c5366e
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-25 15:19:25 -0300

    exercise 2 part 1 done

--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercise1part1.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercise1part1.js
@@ -1,4 +1,4 @@
-/* A função sum(a, b) retorna a soma do parâmetro a com o b */
+/* 1. A função sum(a, b) retorna a soma do parâmetro a com o b */
 
 const assert = require('assert');
 
@@ -10,6 +10,12 @@ function sum(a, b) {
   return a + b;
 }
 
-const expected = sum(2, 2);
+// const expected = sum(2, 2);
 
-assert.strictEqual(expected, 4, '2 + 2 = 4');
+// assert.strictEqual(expected, 9, '2 + 2 = 4');
+
+/* 2. Teste se o retorno de sum(4, 5) é 9 */
+
+const expected = sum(4, 5);
+
+assert.strictEqual(expected, 9, '4 + 5 = 9');

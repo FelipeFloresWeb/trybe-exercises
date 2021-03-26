468efbc965f3d510bb6bbfdd24d44b0b4bb78a4b
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-26 09:19:58 -0300

    exercise 2 part 3 Done

--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart3.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart3.js
@@ -4,20 +4,51 @@ const assert = require('assert');
 escreva testes para verificar se a mesma está retornando como se vê na
 variável result e, caso não esteja, altere o código para que ele passe nos testes. */
 
-const greetPeople = (people) => {
-  const greeting = 'Hello ';
-  const result = [];
-  for (const value of people) {
-    const text = `${greeting}${value}`;
-    result.push(text);
+// const greetPeople = (people) => {
+//   const greeting = 'Hello ';
+//   const result = [];
+//   for (const value of people) {
+//     const text = `${greeting}${value}`;
+//     result.push(text);
+//   }
+//   return result;
+// };
+
+// const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+// const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+// const output = greetPeople(parameter);
+// assert.strictEqual(typeof (greetPeople), 'function');
+// assert.deepStrictEqual(output, result, 'Error: The array has not been changed');
+
+//  2. Use a variável parameter como parâmetro da função abaixo,
+// escreva testes para verificar se a mesma está retornando como se vê na
+// variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
+const removeVowels = (word) => {
+  let results = '';
+  let numberOfVogals = 0;
+  const characters = word.split('');
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a'
+      || characters[index] === 'o'
+      || characters[index] === 'i'
+      || characters[index] === 'e'
+      || characters[index] === 'u'
+    ) {
+      numberOfVogals += 1;
+      results += numberOfVogals;
+    } else {
+      results += characters[index];
+    }
   }
-  return result;
+  return results;
 };
 
-const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+const parameter = 'Dayane';
+const result = 'D1y2n3';
 
-const output = greetPeople(parameter);
-assert.strictEqual(typeof (greetPeople), 'function');
-assert.deepStrictEqual(output, result, 'Error: The array has not been changed');
-// assert.deepStrictEqual(myArray, unchanged);
+const output = removeVowels(parameter);
+assert.strictEqual(typeof (removeVowels), 'function');
+assert.strictEqual(output, result, 'Error: The array has not correct changed');

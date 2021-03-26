937dc10ccc940c1ae28aa4e78701780de1673acb
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-26 10:27:14 -0300

    Exercise 4 part 3 Done

--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart3.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart3.js
@@ -59,22 +59,43 @@ variável result e, caso não esteja, altere o código para que ele passe nos te
 escreva testes para verificar se a mesma está retornando como se vê
 na variável result e, caso não esteja, altere o código para que ele passe nos testes. */
 
-const greaterThanTen = (array) => {
+// const greaterThanTen = (array) => {
+//   const results = [];
+//   for (let index = 0; index < array.length; index += 1) {
+//     if (typeof (array[index]) !== 'number') {
+//       throw new Error('Enter only numbers');
+//     }
+//     if (array[index] > 10) {
+//       results.push(array[index]);
+//     }
+//   }
+//   return results;
+// };
+
+// const parameter = [4, 10, 32, 9, 21];
+// const result = [32, 21];
+
+// const output = greaterThanTen(parameter);
+// assert.deepStrictEqual(typeof (greaterThanTen), 'function');
+// assert.deepStrictEqual(output, result, 'Error: The array has not correct changed');
+
+/* 4. Use a variável parameter como parâmetro da função abaixo,
+escreva testes para verificar se a mesma está retornando como se vê
+na variável result e, caso não esteja, altere o código para que ele passe nos testes. */
+
+const secondThirdSmallest = (array) => {
   const results = [];
-  for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) !== 'number') {
-      throw new Error('Enter only numbers');
-    }
-    if (array[index] > 10) {
-      results.push(array[index]);
-    }
-  }
+
+  array.sort((x, y) => x - y);
+
+  results.push(array[1]);
+  results.push(array[2]);
   return results;
 };
 
-const parameter = [4, 10, 32, 9, 21];
-const result = [32, 21];
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
 
-const output = greaterThanTen(parameter);
-assert.deepStrictEqual(typeof (greaterThanTen), 'function');
+const output = secondThirdSmallest(parameter);
+assert.deepStrictEqual(typeof (secondThirdSmallest), 'function');
 assert.deepStrictEqual(output, result, 'Error: The array has not correct changed');

553b5b0f3549e41ab96a873ebb82b782e262e3ee
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-25 23:04:38 -0300

    Exercise 2 part 2 done

--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart2.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart2.js
@@ -3,21 +3,39 @@
 
 const assert = require('assert');
 
-const addOne = (array) => {
+// const addOne = (array) => {
+//   const arrayOutput = [];
+//   for (let index = 0; index < array.length; index += 1) {
+//     let element = array[index];
+//     element += 1;
+//     arrayOutput.push(element);
+//   }
+//   return arrayOutput;
+// };
+
+// const myArray = [31, 57, 12, 5];
+// const unchanged = [31, 57, 12, 5];
+// const expected = [32, 58, 13, 6];
+// const output = addOne(myArray);
+
+// assert.strictEqual(typeof addOne, 'function');
+// assert.deepStrictEqual(output, expected);
+// assert.deepStrictEqual(myArray, unchanged);
+
+// 2. Escreva a função wordLengths para passar nos testes já implementados.
+
+const wordLengths = (array) => {
   const arrayOutput = [];
   for (let index = 0; index < array.length; index += 1) {
-    let element = array[index];
-    element += 1;
-    arrayOutput.push(element);
+    const element = array[index];
+    arrayOutput.push(element.length);
   }
   return arrayOutput;
 };
 
-const myArray = [31, 57, 12, 5];
-const unchanged = [31, 57, 12, 5];
-const expected = [32, 58, 13, 6];
-const output = addOne(myArray);
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
 
-assert.strictEqual(typeof addOne, 'function');
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged);

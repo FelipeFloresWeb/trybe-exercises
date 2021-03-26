e713caeebe4a7de16845a45a563d5a5367664b31
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-25 22:47:50 -0300

    Exercise 1 part 2 Done and corrext error lint part 1 exercises

--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart1.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart1.js
@@ -158,10 +158,10 @@ const obj1 = {
   description: 'My Description',
 };
 
-const obj2 = {
-  description: 'My Description',
-  title: 'My Title',
-};
+// const obj2 = {
+//   description: 'My Description',
+//   title: 'My Title',
+// };
 
 const obj3 = {
   title: 'My Different Title',
--- FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart2.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia_3/exercisespart2.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-console */
 // 1. Escreva a função addOne para passar nos testes já implementados.
 
 const assert = require('assert');
 
-const addOne = () => console.log('one function');
+const addOne = (array) => {
+  const arrayOutput = [];
+  for (let index = 0; index < array.length; index += 1) {
+    let element = array[index];
+    element += 1;
+    arrayOutput.push(element);
+  }
+  return arrayOutput;
+};
 
 const myArray = [31, 57, 12, 5];
 const unchanged = [31, 57, 12, 5];

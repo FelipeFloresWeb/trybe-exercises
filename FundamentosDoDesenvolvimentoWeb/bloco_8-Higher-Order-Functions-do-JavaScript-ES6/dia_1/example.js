08205fc46886ebab0cdfc6ed8820d059e4615879
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-28 07:01:31 -0300

    example 1 done

---
+++ FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_1/example.js
@@ -0,0 +1,9 @@
+// Ao chamar a função doingThings:
+// doingThings(wakeUp);
+
+// Ela deve retornar o valor do respectivo parâmetro, neste caso:
+// Acordando!!
+
+// Crie uma função de primeira classe que tenha o retorno console.log('Acordando!!') ;
+
+const wakeUp = () => console.log('Acordando!!');

bc784a6c3bb4633af99ca94307c45099f3a95f4d
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-28 07:35:48 -0300

    added example of function doingThing

--- FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_1/example.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_1/example.js
@@ -5,7 +5,22 @@
 // Acordando!!
 
 // Crie uma função de primeira classe que tenha o retorno console.log('Acordando!!') ;
-
-const wakeUp = () => console.log('Acordando!!');
-
+function wakeUp() {
+  return console.log('Acordando!!');
+}
+// Crie outra função de primeira classe que tenha o retorno console.log('Bora tomar café!!') ;
 const breakfast = () => console.log('Bora tomar café!!');
+
+// Crie mais uma função de primeira classe que tenha o retorno console.log('Partiu dormir!!') ;
+const goSleep = () => console.log('Partiu dormir!!');
+
+/* Desenvolva uma HOF chamada doingThings e configure esta função para que retorne a execução das
+funções de primeira classe que você criou nos exemplos anteriores. */
+
+// const printAnything = () => console.log();
+
+const doingThings = (whichthing) => whichthing();
+
+doingThings(wakeUp);
+doingThings(breakfast);
+doingThings(goSleep);

e4329014af0824565ed839b55aa6a05c51d7bc83
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-29 11:24:56 -0300

    added some examples

--- FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_2/example.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_2/example.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Método for each:
 
 const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
@@ -28,3 +29,46 @@ arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
 element - Valor do elemento do array;
 index - Índice em cada iteração ou posição do elemento no array, começando do 0;
 array - Array original que está sendo percorrido. */
+
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Use o método forEach chamando a callback showEmailList para apresentar os emails.
+
+showEmailList(emailListInData.forEach((el) => {
+  console.log(`O email ${el} esta cadastrado em nosso banco de dados!`);
+}));
+
+/* Para fixar, você pode praticar com este exemplo do forEach feito no CodePen.
+Agora vamos usar o forEach , para realizar a tabuada do 2. Veja o exemplo abaixo:
+Copiar */
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+/* No exemplo acima, foi executado para cada elemento do array a função multipliesFor2,
+que imprime o parâmetro element * 2 no console.
+Agora estamos tratando de uma HOF , sendo assim é possível se utilizar também dos
+demais parâmetros para se resolver um problema. Como se pode fazer isso? Veja este
+exemplo abaixo com o uso de index no forEach : */
+
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]

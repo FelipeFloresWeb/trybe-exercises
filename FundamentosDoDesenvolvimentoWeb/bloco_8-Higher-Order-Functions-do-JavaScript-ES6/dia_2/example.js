fca30ce944beb71a778383515362e2303378c639
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-29 11:05:11 -0300

    Added new folder with exercises of day 8.2

---
+++ FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_2/example.js
@@ -0,0 +1,30 @@
+// Método for each:
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', indexOfTheArray);
+  console.log('Array percorrido:', theEntireArray);
+});
+
+// => ---------------
+//   Cada elemento do array: josé
+//   Index, posição do elemento: 0
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 50
+//   Index, posição do elemento: 1
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 0.25
+//   Index, posição do elemento: 2
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: { comida: 'Chocolate' }
+//   Index, posição do elemento: 3
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+
+/* A arrow function passada para o forEach possui element , index e array como parâmetros, onde:
+element - Valor do elemento do array;
+index - Índice em cada iteração ou posição do elemento no array, começando do 0;
+array - Array original que está sendo percorrido. */

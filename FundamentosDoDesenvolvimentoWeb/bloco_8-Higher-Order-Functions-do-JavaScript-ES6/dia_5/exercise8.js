4898305dcccd5743d5fa18383f3cd367aa1d68ac
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-07 07:53:29 -0300

    exercise 8 done

---
+++ FundamentosDoDesenvolvimentoWeb/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_5/exercise8.js
@@ -0,0 +1,15 @@
+/* Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+Dica: use default params . */
+
+const assert = require('assert');
+
+// escreva greet abaixo
+
+const greet = (name, greeting) => {
+  const defaultGreeting = typeof greeting === 'undefined' ? 'Hi' : greeting;
+  return `${defaultGreeting} ${name}`;
+};
+
+assert.strictEqual(greet('John'), 'Hi John');
+assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
+assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');

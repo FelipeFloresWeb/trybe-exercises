7006c77b0a6ec2013a1ae7803f672e19da5877dc
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-12 15:43:00 -0300

    Exercise 3 Init

--- FundamentosDoDesenvolvimentoWeb/bloco_9-JavaScript-Assincrono-e-Promises/exercises.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_9-JavaScript-Assincrono-e-Promises/exercises.js
@@ -34,32 +34,48 @@
 
 /* 2. Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
  */
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) => `${name} is ${value} ${measurementUnit} apart from the Sun`;
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) => `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-const mars = {
-  name: 'Mars',
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: 'kilometers',
-  },
+// const mars = {
+//   name: 'Mars',
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: 'kilometers',
+//   },
+// };
+
+// const venus = {
+//   name: 'Venus',
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: 'kilometers',
+//   },
+// };
+
+// const jupiter = {
+//   name: 'Jupiter',
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: 'kilometers',
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// setTimeout(() => console.log(planetDistanceFromSun(venus)), 2000); // B
+// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 3000); // C
+
+/* 3. A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet ,
+de forma que Marte seja impresso assincronamente, depois de 4 segundos. */
+
+const getPlanet = () => {
+  const mars = {
+    name: 'Mars',
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: 'kilometers',
+    },
+  };
+  console.log('Returned planet: ', mars);
 };
 
-const venus = {
-  name: 'Venus',
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: 'kilometers',
-  },
-};
-
-const jupiter = {
-  name: 'Jupiter',
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: 'kilometers',
-  },
-};
-
-console.log(planetDistanceFromSun(mars)); // A
-setTimeout(() => console.log(planetDistanceFromSun(venus)), 2000); // B
-setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 3000); // C
+getPlanet(); // imprime Marte depois de 4 segundos

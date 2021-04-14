b63d11c2310c78f33d4db9bcff8d40f8f40bf014
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-14 00:46:30 -0300

    Exercises and bonus Done

--- FundamentosDoDesenvolvimentoWeb/bloco_9-JavaScript-Assincrono-e-Promises/dia_2/apiScript.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_9-JavaScript-Assincrono-e-Promises/dia_2/apiScript.js
@@ -1,5 +1,32 @@
 /* eslint-disable no-console */
 const API_URL = 'https://icanhazdadjoke.com/';
+const fetch = require('node-fetch');
+
+const num = () => Math.floor((Math.random() * 51) ** 2);
+const arr = [num(), num(), num(), num(), num(), num(), num(), num(), num(), num()];
+const sumOfArr = arr.reduce((acc, item) => acc + item, 0);
+
+const divBy = (number) => {
+  const output = [2, 3, 5, 10];
+  const arroutput = output.map((el) => parseInt((number / el), 10));
+  return arroutput.reduce((acc, item) => acc + item, 0);
+};
+
+const thePromisse = (element) => new Promise((resolve, reject) => {
+  if (element > 8000) {
+    reject(new Error('É mais de oito mil! Essa promise deve estar quebrada!'));
+  } else {
+    resolve(console.log(divBy(element)));
+  }
+});
+const fetchThePromisse = async () => {
+  try {
+    await thePromisse(sumOfArr);
+  } catch (error) {
+    console.log(error);
+  }
+};
+fetchThePromisse();
 
 const createJokeText = (obj) => {
   const selectH2 = document.querySelector('h2');
@@ -22,10 +49,7 @@ window.onload = () => fetchJoke();
 
 // const translatedweord = async () => {
 //   try {
-//     await fetchJoke(param);
-//     await fetchJoke(param);
-//     await fetchJoke(param);
-//     await fetchJoke(param);
+//     await chJoke(param);
 //     await fetchJoke(param);
 //   } catch (error) {
 //     console.log(error);

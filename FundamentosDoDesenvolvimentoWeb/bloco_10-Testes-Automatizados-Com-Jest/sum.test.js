ee50160c9a4b9a08360ab553ef07ef0d019bc14e
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-16 13:10:25 -0300

    exercise 1 Done

--- FundamentosDoDesenvolvimentoWeb/bloco_10-Testes-Automatizados-Com-Jest/sum.test.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_10-Testes-Automatizados-Com-Jest/sum.test.js
@@ -8,14 +8,18 @@ Teste se a mensagem de erro é "parameters must be numbers" quando realizar a ch
 
 describe('Função sum()', () => {
   test('Verifica se ao somar 4 + 5 retorna 9.', () => {
-    expect(sum(4, 5)).toEqual(9);
+    expect(sum(4, 5)).toBe(9);
   });
 
   test('Verifica se ao somar 0 + 0 retorna 0.', () => {
-    expect(sum(0, 0)).toEqual(0);
+    expect(sum(0, 0)).toBe(0);
   });
 
   test('Verifica se ao passar uma string, a função devolve um erro', () => {
-    expect(sum(4, '5')).toThrow(Error);
+    expect(() => sum(4, '5')).toThrow(Error);
+  });
+
+  test('Verifica se a mensagem de erro é igual a: ("parameters must be numbers")', () => {
+    expect(() => sum(4, '5')).toThrow('parameters must be numbers');
   });
 });

a25c0878a1f1a62620d235ff38b9f04e597bea23
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-18 00:54:43 -0300

    First part Done (added all imgs with champions and arquives). Ok website behavior

---
+++ FundamentosDoDesenvolvimentoWeb/firstGameProject/script.js
@@ -0,0 +1,220 @@
+// const earthData = require('./championsImgs');
+const champions = {
+  Aatrox: 'url(img/champion/splash/Aatrox_0.jpg)',
+  Ahri: 'url(img/champion/splash/Ahri_0.jpg)',
+  Akali: 'url(img/champion/splash/Akali_0.jpg)',
+  Alistar: 'url(img/champion/splash/Alistar_0.jpg)',
+  Amumu: 'url(img/champion/splash/Amumu_0.jpg)',
+  Anivia: 'url(img/champion/splash/Anivia_0.jpg)',
+  Annie: 'url(img/champion/splash/Annie_0.jpg)',
+  Aphelios: 'url(img/champion/splash/Aphelios_0.jpg)',
+  Ashe: 'url(img/champion/splash/Ashe_0.jpg)',
+  AurelionSol: 'url(img/champion/splash/AurelionSol_0.jpg)',
+  Azir: 'url(img/champion/splash/Azir_0.jpg)',
+  Bard: 'url(img/champion/splash/Bard_0.jpg)',
+  Blitzcrank: 'url(img/champion/splash/Blitzcrank_0.jpg)',
+  Brand: 'url(img/champion/splash/Brand_0.jpg)',
+  Braum: 'url(img/champion/splash/Braum_0.jpg)',
+  Caitlyn: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Camille: 'url(img/champion/splash/Camille_0.jpg)',
+  Cassiopeia: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Chogath: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Corki: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Darius: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Diana: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Draven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  DrMundo: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Ekko: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Elise: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Evelynn: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Ezreal: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Fiddlesticks: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Fiora: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Fizz: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Galio: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Gangplank: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Garen: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Gnar: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Gragas: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Graves: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Gwen: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Hecarim: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Heimerdinger: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Illaoi: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Irelia: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Ivern: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Janna: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  JarvanIV: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Jax: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Jayce: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Jhin: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Jinx: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kaisa: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kalista: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Karma: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Karthus: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kassadin: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Katarina: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kayle: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kayn: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kennen: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Khazix: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kindred: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Kled: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  KogMaw: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Leblanc: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  LeeSin: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Leona: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Lillia: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Lissandra: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Lucian: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Lulu: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Lux: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Malphite: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Malzahar: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Maokai: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  MasterYi: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  MissFortune: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  MonkeyKing: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Mordekaiser: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Morgana: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Nami: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Nasus: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Nautilus: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Neeko: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Nidalee: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Nocturne: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Nunu: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Olaf: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Orianna: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Ornn: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Pantheon: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Poppy: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Pyke: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Qiyana: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Quinn: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Rakan: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Rammus: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  RekSai: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Rell: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Renekton: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Rengar: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+  Riven: 'url(img/champion/splash/Caitlyn_0.jpg)',
+};
+
+const championsCards = {
+  Aatrox: 'img/champion/loading/Aatrox_0.jpg',
+  Ahri: 'img/champion/loading/Ahri_0.jpg',
+  Akali: 'img/champion/loading/Akali_0.jpg',
+  Alistar: 'img/champion/loading/Alistar_0.jpg',
+  Amumu: 'img/champion/loading/Amumu_0.jpg',
+  Anivia: 'img/champion/loading/Anivia_0.jpg',
+  Annie: 'img/champion/loading/Annie_0.jpg',
+  Aphelios: 'img/champion/loading/Aphelios_0.jpg',
+  Ashe: 'img/champion/loading/Ashe_0.jpg',
+  AurelionSol: 'img/champion/loading/AurelionSol_0.jpg',
+  Azir: 'img/champion/loading/Azir_0.jpg',
+  Bard: 'img/champion/loading/Bard_0.jpg',
+  Blitzcrank: 'img/champion/loading/Blitzcrank_0.jpg',
+  Brand: 'img/champion/loading/Brand_0.jpg',
+  Braum: 'img/champion/loading/Braum_0.jpg',
+  Caitlyn: 'img/champion/loading/Caitlyn_0.jpg',
+};
+
+function fetchResponse() {
+  return fetch('http://ddragon.leagueoflegends.com/cdn/11.8.1/data/pt_BR/champion.json')
+    .then((data) => data.json())
+    .then((champion) => champion.data);
+}
+
+const showchamp = (fighter) => {
+  const selectImages = document.querySelectorAll('.championscards');
+  const selectChampText = document.querySelector('#cardText');
+
+  if (selectImages.length > 0) {
+    selectImages[0].remove();
+    selectChampText.remove();
+  }
+
+  const selectBody = document.body;
+  selectBody.style.backgroundImage = champions[fighter.id];
+  selectBody.style.transition = '1.5s';
+
+  const selectDivChampCards = document.querySelector('#champCards');
+  const createImg = document.createElement('img');
+  createImg.classList.add('championscards');
+  createImg.src = championsCards[fighter.id];
+  selectDivChampCards.appendChild(createImg);
+
+  const createSpan = document.createElement('span');
+  createSpan.id = 'cardText';
+  createSpan.style.backgroundColor = 'black';
+  createSpan.innerHTML = `${fighter.name} "${fighter.title}".<br><br>${fighter.blurb}<br><br>
+  Ataque Pontos de Vida: ${fighter.stats.hp}<br>
+  Ataque Físico: ${fighter.stats.attackdamage}<br>
+  Velocidade de Ataque: ${fighter.stats.attackspeed}ms<br>
+  Defesa Física: ${fighter.stats.armor}<br>
+  Defesa Mágica: ${fighter.stats.spellblock}<br>
+  Distancia do Ataque: ${(fighter.stats.attackrange < 500)
+    ? fighter.stats.attackrange * 0.01
+    : fighter.stats.attackrange * 0.1}metros<br>
+    Defesa Física por nível: ${fighter.stats.armorperlevel} pontos<br>
+    Defesa Mágica por nível: ${fighter.stats.armorperlevel} pontos<br>
+    Pontos de vida por nível: ${fighter.stats.spellblock} pontos<br>
+    Classe: ${fighter.tags.toString().replace(',', ', ')}`;
+
+  selectDivChampCards.appendChild(createSpan);
+};
+
+const selectChamp = (event, champion) => {
+  const champ = event.target;
+  champ.classList.add('selected');
+  showchamp(champion);
+  setTimeout(() => champ.classList.remove('selected'), 1000);
+  champ.classList.add('deSelected');
+  setTimeout(() => champ.classList.remove('deSelected'), 2000);
+};
+
+const showChampionsNames = (champions1) => {
+  champions1.forEach((champion) => {
+    const selectDivChamps = document.querySelector('#champs');
+    const createSpan = document.createElement('span');
+    createSpan.innerHTML = champion.name;
+    createSpan.classList.add('champion');
+    // createSpan.style.backgroundImage = `url(img/champion/tiles/${champion.image.full})`;
+    // createSpan.style.backgroundSize = '100%';
+    createSpan.addEventListener('click', (event) => selectChamp(event, champion));
+    selectDivChamps.appendChild(createSpan);
+    // const createImg = document.createElement('img');
+    // createImg.classList.add('cardicon');
+    // createImg.src = `img/champion/tiles/${champion.image.full}`;
+    // selectDivChamps.appendChild(createImg);
+  });
+};
+
+window.onload = async function onload() {
+  try {
+    const champions0 = await fetchResponse();
+    const arrchampions = Object.values(champions0);
+    showChampionsNames(arrchampions);
+    console.log(arrchampions);
+  } catch (error) {
+    alert(error);
+  }
+};

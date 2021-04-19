161efb756d78b8ce2b65da00685cb73c134a5895
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-19 18:57:06 -0300

    corrigido estrutura pastas

--- FundamentosDoDesenvolvimentoWeb/bloco_10-Testes-Automatizados-Com-Jest/dia_1/tests/part1.test.js
+++ FundamentosDoDesenvolvimentoWeb/bloco_10-Testes-Automatizados-Com-Jest/dia_1/tests/part1.test.js
@@ -1,10 +1,10 @@
-const sum = require('./dia_1/sum');
-const myRemove = require('./dia_1/myRemove');
-const myRemoveWithoutCopy = require('./dia_1/myRemoveWithoutCopy');
+const sum = require('../functions/sum');
+const myRemove = require('../functions/myRemove');
+const myRemoveWithoutCopy = require('../functions/myRemoveWithoutCopy');
 
 /* 1. A função sum(a, b) retorna a soma do parâmetro a com o b */
 
-describe('Função sum() and myremove()', () => {
+describe('Função sum()', () => {
   // Teste se o retorno de sum(4, 5) é 9 *
   test('Verifica se ao somar 4 + 5 retorna 9.', () => {
     expect(sum(4, 5)).toBe(9);
@@ -17,7 +17,7 @@ describe('Função sum() and myremove()', () => {
 
   // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
   test('Verifica se ao passar uma string, a função devolve um erro', () => {
-    expect(() => sum(4, '5')).toThrow(Error);
+    expect(() => sum(4, '5')).toThrow();
   });
 
   // Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada
@@ -25,11 +25,11 @@ describe('Função sum() and myremove()', () => {
   test('Verifica se a mensagem de erro é igual a: ("parameters must be numbers")', () => {
     expect(() => sum(4, '5')).toThrow('parameters must be numbers');
   });
-
-  /* 2. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o
+});
+/* 2. A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o
 elemento item caso ele exista no array
 */
-
+describe('Função myRemove()', () => {
   // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
   test('Verifique se a chamada myRemove([1, 2, 3, 4]) retorna 3', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
@@ -50,12 +50,11 @@ elemento item caso ele exista no array
     expect(() => myRemove([1, 2, 3, 4], 5)).not.toContainEqual(5);
   });
 
-  /* 3. A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o
-  próprio array sem o elemento item caso ele exista no array */
+  //  3. A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o
+  // próprio array sem o elemento item caso ele exista no array
 
   // Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
-
-  // test('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
-  //   expect(() => myRemove([1, 2, 3, 4], 5)).toContain([1, 2, 3, 4]);
-  // });
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(() => myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual(3);
+  });
 });

8c27c5f3819f1191389a4b1921b577287d0b95b7
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-22 18:56:14 -0300

    exercise 4 and 5 done

---
+++ FundamentosDoDesenvolvimentoWeb/bloco_10-Testes-Automatizados-Com-Jest/dia_3/src/exercise4.js
@@ -0,0 +1,7 @@
+const stringToUpperCase = (string) => string.toUpperCase();
+
+const theFirstLetter = (string) => string.split('')[0];
+
+const sumStrings = (string1, string2) => `${string1} ${string2}`;
+
+module.exports = { stringToUpperCase, theFirstLetter, sumStrings };

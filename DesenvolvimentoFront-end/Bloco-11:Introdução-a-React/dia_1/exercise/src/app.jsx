ab74c125999e23a9301fed608b997c2ac127a7bd
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-26 14:38:59 -0300

    front end development started, created new folder and added example and exercises

---
+++ DesenvolvimentoFront-end/Bloco-11:Introdução-a-React/dia_1/exercise/src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import task from './task';
+
+function myApp(params) {
+  return {
+    task('Beber água'),
+    task('Correr'),
+    task('Tomar Banho'),
+    task('Café'),
+    task('Estudar'),
+    task('Almoço'),
+  };
+}
---
+++ DesenvolvimentoFront-end/Bloco-11:Introdução-a-React/dia_1/exercise/src/task.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+export default task;

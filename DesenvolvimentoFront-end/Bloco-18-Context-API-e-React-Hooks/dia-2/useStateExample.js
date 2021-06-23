9bb19887a2e0c4b0e44aa5b2990488b3b79f7dcb
Felipe Flores <felipefloresvendas@gmail.com>
2021-06-23 10:56:43 -0300

    feat: new folder of day 2, with useState example

---
+++ DesenvolvimentoFront-end/Bloco-18-Context-API-e-React-Hooks/dia-2/useStateExample.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <div>Counter: {counter}</div>
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        Increase Counter
+      </button>
+    </div>
+  );
+}
+
+export default App;

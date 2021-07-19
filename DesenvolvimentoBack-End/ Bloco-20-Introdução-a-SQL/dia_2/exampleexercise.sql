4e957ba0ad8fc18f9545958a2b42192211ef133d
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-19 20:26:31 -0300

    feat: added example about LIMIT

--- DesenvolvimentoBack-End/ Bloco-20-Introdução-a-SQL/dia_2/exampleexercise.sql
+++ DesenvolvimentoBack-End/ Bloco-20-Introdução-a-SQL/dia_2/exampleexercise.sql
@@ -82,3 +82,22 @@ SELECT COUNT(active) FROM staff;
 --Quantos emails temos cadastrados nessa tabela?
 USE sakila;
 SELECT COUNT(email) FROM staff;
+
+
+
+
+
+
+--Pesquisas gigantes? LIMIT isso ae!
+--Se você abrir agora o nosso banco de dados de prática sakila e executar a query a seguir, verá que o resultado é 16044, ou seja, existem 16044 linhas na tabela rental .
+
+SELECT COUNT(*) FROM sakila.rental;
+
+--Uma curiosidade é que, usando o MySQL Workbench , caso você faça uma consulta que inclui todas as linhas da tabela e rolar a listagem até o fim, verá que o resultado da consulta são 16044 linhas (desde que o Workbench não limite os resultados da sua consulta, como você verá na próxima seção).
+
+SELECT * FROM sakila.rental;
+
+--Isso não é sempre necessário e pode até ser um problema em bancos de dados gigantes, em que as tabelas podem conter milhares ou milhões de linhas. Resolver isso é bem simples: tudo que você precisa fazer é limitar o resultado usando o LIMIT :
+
+--# Query + LIMIT quantidade_de_resultados
+SELECT * FROM sakila.rental LIMIT 10;

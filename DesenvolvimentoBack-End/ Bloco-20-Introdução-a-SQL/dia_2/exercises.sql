305b85df9ffe1727034ec881f5ad8bd59b3b044b
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-19 22:16:09 -0300

    feat: added exercises

---
+++ DesenvolvimentoBack-End/ Bloco-20-Introdução-a-SQL/dia_2/exercises.sql
@@ -0,0 +1,43 @@
+--Escreva uma query para exibir a string "This is SQL Exercise, Practice and Solution".
+
+
+--Escreva uma query para exibir três números em três colunas.
+
+
+--Escreva uma query para exibir a soma dos números 10 e 15.
+
+
+--Escreva uma query para exibir o resultado de uma expressão aritmética qualquer.
+
+
+--Escreva uma query para exibir todas as informações de todos os cientistas.
+
+
+--Escreva uma query para exibir o nome como "Nome do Projeto" e as horas como "Tempo de Trabalho" de cada projeto.
+
+
+--Escreva uma query para exibir o nome dos cientistas em ordem alfabética.
+
+
+--Escreva uma query para exibir o nome dos Projetos em ordem alfabética descendente.
+
+
+--Escreva uma query que exiba a string "O projeto Name precisou de Hours horas para ser concluído." para cada projeto.
+
+
+--Escreva uma query para exibir o nome e as horas dos três projetos com a maior quantidade de horas.
+
+
+--Escreva uma query para exibir o código de todos os projetos da tabela AssignedTo sem que haja repetições.
+
+
+--Escreva uma query para exibir o nome do projeto com maior quantidade de horas.
+
+
+--Escreva uma query para exibir o nome do segundo projeto com menor quantidade de horas.
+
+
+--Escreva uma query para exibir todas as informações dos cinco projetos com a menor quantidade de horas.
+
+
+--Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas.

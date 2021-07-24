733b08354df69981c630d7666a795be8419253da
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-24 13:02:51 -0300

    feat: added new method example of if and case

---
+++ DesenvolvimentoBack-End/Bloco-21-Funcoes-SQL-Joins-e-Subqueries/dia_1/condicionaisIF-CASE.sql
@@ -0,0 +1,48 @@
+--Condicionais
+
+--Em linguagens de alto nível como Python , JavaScript e outras, as condicionais são a base para a criação de algorítimos dinâmicos que se adaptam de acordo com a necessidade do programa. O SQL não fica para trás nesse quesito, sendo possível nele também usar os principais comandos de controle de fluxo, como o IF e o CASE .
+--Obs: Caso queria saber mais sobre linguagens de alto nível, leia o artigo de William Oliveira na sessão de recusos adicionais.
+-- Sintaxe:
+SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
+
+SELECT IF(idade >= 18, 'Maior de idade', 'Menor de Idade')
+FROM pessoas;
+
+SELECT IF(aberto, 'Entrada permitida', 'Entrada não permitida')
+FROM estabelecimentos;
+
+-- Exemplo utilizando o banco sakila:
+SELECT first_name, IF(active, 'Cliente Ativo', 'Cliente Inativo') AS status
+FROM sakila.customer
+LIMIT 20;
+--Em situações em que é preciso comparar mais de uma condição, é preferível utilizar o CASE .
+
+-- Sintaxe:
+SELECT CASE
+  WHEN condicao THEN valor
+  ELSE valor padrao
+END;
+
+SELECT
+    nome,
+    nivel_acesso,
+    CASE
+        WHEN nivel_acesso = 1 THEN 'Nível de acesso 1'
+        WHEN nivel_acesso = 2 THEN 'Nível de acesso 2'
+        WHEN nivel_acesso = 3 THEN 'Nível de acesso 3'
+        ELSE 'Usuário sem acesso'
+    END AS nivel_acesso
+FROM permissoes_usuario;
+
+-- Exemplo utilizando a tabela sakila.film:
+SELECT
+    first_name,
+    email,
+    CASE
+        WHEN email = 'MARY.SMITH@sakilacustomer.org' THEN 'Cliente de baixo valor'
+        WHEN email = 'PATRICIA.JOHNSON@sakilacustomer.org' THEN 'Cliente de médio valor'
+        WHEN email = 'LINDA.WILLIAMS@sakilacustomer.org' THEN 'Cliente de alto valor'
+        ELSE 'não classificado'
+    END AS valor
+FROM sakila.customer
+LIMIT 10;

2252318c3eff8d72f9061fc18589b05d2b969d47
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-26 19:00:35 -0300

    feat: Added exercises example of SELF JOIN

--- DesenvolvimentoBack-End/Bloco-21-Funcoes-SQL-Joins-e-Subqueries/dia_2/SELFJOIN.sql
+++ DesenvolvimentoBack-End/Bloco-21-Funcoes-SQL-Joins-e-Subqueries/dia_2/SELFJOIN.sql
@@ -5,11 +5,17 @@
 --Note que um SELF JOIN não é um tipo diferente de JOIN . É apenas um caso em que uma tabela faz join consigo mesma. Você pode utilzar qualquer dos tipos de JOIN vistos ao realizar um SELF JOIN .
 --Como visto no vídeo, utilizando o schema sakila como exemplo, se quisermos buscar o título e o custo de substitução ( replacement_cost ) dos filmes que possuem a mesma duração, podemos montar a seguinte query usando SELF JOIN :
 
-SELECT t1.title, t1.replacement_cost, t2.title, t2.replacement_cost
-FROM sakila.film AS t1, sakila.film AS t2
-WHERE t1.length = t2.length;
 --Para fixar esses conceitos, tente encontrar as seguintes informações:
 --Queremos saber os ids e custos de substituição dos filmes que possuem o mesmo custo de substituição.
+SELECT t1.film_id, t1.replacement_cost
+FROM sakila.film AS t1, sakila.film AS t2
+WHERE t1.replacement_cost = t2.replacement_cost;
+
 --Exiba o título e a duração de empréstimo dos filmes que possuem a mesma duração. Exiba apenas os filmes com a duração de empréstimo entre 2 e 4 dias.
+
+SELECT t1.title, t1.rental_duration
+FROM sakila.film AS t1, sakila.film AS t2
+WHERE t1.length = t2.length;
+
 --Lembre-se de usar o SELF JOIN em situações em que as informações que estiver buscando estejam armazenadas em apenas uma tabela.
 --A seguir, veremos como é possível combinar os resultados de uma query com outra por meio do UNION .

1ae19c2fed90c52cfb61f40698fcb5cc9693e40e
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-27 21:11:58 -0300

    Added Trigger example

--- DesenvolvimentoBack-End/Bloco-21-Funcoes-SQL-Joins-e-Subqueries/dia_3/storeProcedure.sql
+++ DesenvolvimentoBack-End/Bloco-21-Funcoes-SQL-Joins-e-Subqueries/dia_3/storeProcedure.sql
@@ -244,3 +244,17 @@ SELECT GetFullName(51);
 --Utilizando a tabela sakila.payment , monte uma function que retorna a quantidade total de pagamentos feitos até o momento por um determinado customer_id .
 --Crie uma function que, dado o parâmetro de entrada inventory_id , retorna o nome do filme vinculado ao registro de inventário com esse id.
 --Crie uma function que receba uma determinada categoria de filme em formato de texto (ex: 'Action' , 'Horror' ) e retorna a quantidade total de filmes registrados nessa categoria.
+
+
+
+
+
+--Stored Functions VS Store Procedures
+
+--Functions VS Procedures
+--Ou seja:
+--Stored Procedures podem ser chamadas através do comando CALL , e o retorno de um valor é opcional;
+--Já as Stored Functions são executadas com o comando SELECT e obrigatoriamente retornam algum valor;
+--A Stored Functions não pode ser utilizada para alterar o estado global da base dados. (Ex. Por meio das instruções INSERT , UPDATE e DELETE );
+--Stored Procedures permite alterar o estado global.
+--Stored Procedures permitem realizar o tratamento de excepções, via try/catch .

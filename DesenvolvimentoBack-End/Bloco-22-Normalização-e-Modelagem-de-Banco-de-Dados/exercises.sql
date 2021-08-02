a4f0f02631f8ca6b8d3ea5afd88da4f485b84bdf
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-02 18:37:59 -0300

    feat: exercises to 4, added example img  of Match index full text

--- DesenvolvimentoBack-End/Bloco-22-Normalização-e-Modelagem-de-Banco-de-Dados/exercises.sql
+++ DesenvolvimentoBack-End/Bloco-22-Normalização-e-Modelagem-de-Banco-de-Dados/exercises.sql
@@ -21,31 +21,61 @@ CREATE VIEW film_info AS (
   ORDER BY 2
 );
 
---Resultado de exemplo para uma consulta na view do exercício 2
---Crie uma view chamada address_info que faça uso das tabelas address e city do banco de dados sakila . Sua view deve exibir o address_id, o address, o district, o city_id e a city. Os resultados devem ser ordenados pelo nome das cidades. Use a imagem abaixo como referência.
+--Crie uma view chamada address_info que faça uso das tabelas address e city do banco de dados sakila. Sua view deve exibir o address_id, o address, o district, o city_id e a city. Os resultados devem ser ordenados pelo nome das cidades. Use a imagem abaixo como referência.
+USE sakila;
+CREATE VIEW address_info AS (
+  SELECT
+  a.actor_id,
+  CONCAT(a.first_name, ' ', a.last_name) as `actor`,
+  f.title
+  FROM sakila.film AS f
+  JOIN sakila.film_actor AS fa ON f.film_id = fa.film_id
+  JOIN sakila.actor AS a ON fa.actor_id = a.actor_id
+  ORDER BY 2
+);
 
---Resultado de exemplo para uma consulta na view do exercício 3
---Crie uma view chamada movies_languages , usando as tabelas film e language do banco de dados sakila . Sua view deve exibir o título do filme , o id do idioma e o idioma do filme , como na imagem a seguir.
+--Crie uma view chamada movies_languages , usando as tabelas film e language do banco de dados sakila. Sua view deve exibir o título do filme, o id do idioma e o idioma do filme, como na imagem a seguir.
+USE sakila;
+CREATE VIEW movies_languages AS (
+SELECT
+  f.title,
+  l.language_id,
+  l.name
+FROM
+  sakila.film AS f
+INNER JOIN
+  sakila.language AS l ON f.language_id = l.language_id
+);
 
---Resultado de `SELECT * FROM movies_languages`
 --Desafios sobre INDEX
---Verifique o impacto de um FULLTEXT INDEX na tabela category (banco de dados sakila ), adicionando-o na coluna name . Após ter adicionado o índice, mensure o custo da query utilizando o execution plan , como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+--Verifique o impacto de um FULLTEXT INDEX na tabela category (banco de dados sakila), adicionando-o na coluna name. Após ter adicionado o índice, mensure o custo da query utilizando o execution plan , como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+USE sakila;
+CREATE FULLTEXT INDEX categoryIndex ON sakila.category(name);
 
 -- Após ter criado o índice, mensure o custo com a seguinte query:
 SELECT *
 FROM sakila.category
 WHERE MATCH(name) AGAINST('action');
 
+
 -- Após ter excluído o índice, mensure o custo com a seguinte query:
 SELECT *
 FROM sakila.category
 WHERE name LIKE '%action%';
---Verifique o impacto de um INDEX na tabela address (banco de dados sakila ) adicionando-o na coluna postal_code . Após ter adicionado o índice, mensure o custo da query utilizando o execution plan, como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+--Verifique o impacto de um INDEX na tabela address (banco de dados sakila ) adicionando-o na coluna postal_code. Após ter adicionado o índice, mensure o custo da query utilizando o execution plan, como já foi feito em lições anteriores. Após ter criado e mensurado o custo da query, exclua o índice e mensure novamente esse custo.
+
+USE sakila;
+CREATE FULLTEXT INDEX categoryIndex ON sakila.address(postal_code);
 
 -- Mensure o custo com a seguinte query:
 SELECT *
 FROM sakila.address
 WHERE postal_code = '36693';
+
+SELECT *
+FROM sakila.address
+WHERE MATCH(postal_code) AGAINST('36693');
+
 --Desafios sobre ALTER TABLE
 --Restaure o banco de dados HR abaixo antes de continuar, caso não o tenha restaurado em alguma lição anterior:
 --O banco de dados usado como base para os próximos exercícios pode ser restaurado através deste arquivo SQL.

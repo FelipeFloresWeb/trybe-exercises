996f6a83ad9c0102563cd6089d2281d3446dc143
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-02 17:18:11 -0300

    feat: exercise 2

--- DesenvolvimentoBack-End/Bloco-22-Normalização-e-Modelagem-de-Banco-de-Dados/exercises.sql
+++ DesenvolvimentoBack-End/Bloco-22-Normalização-e-Modelagem-de-Banco-de-Dados/exercises.sql
@@ -1,16 +1,28 @@
 --Desafios sobre VIEW
 --Crie uma view chamada film_with_categories utilizando as tabelas category , film_category e film do banco de dados sakila. Essa view deve exibir o título do filme, o id da categoria e o nome da categoria, conforme a imagem abaixo. Os resultados devem ser ordenados pelo título do filme.
+USE sakila;
 CREATE VIEW film_with_categories AS (
-  SELECT f.title, fc.category_id, c.name FROM sakila.film AS f
+  SELECT f.title as film, fc.category_id, c.name FROM sakila.film AS f
   JOIN sakila.film_category AS fc ON f.film_id = fc.film_id
   JOIN sakila.category AS c ON fc.category_id = c.category_id
-  ORDER BY 1;
-)
+  ORDER BY 1
+);
 
---Crie uma view chamada film_info utilizando as tabelas actor , film_actor e film do banco de dados sakila . Sua view deve exibir o actor_id , o nome completo do ator ou da atriz em uma coluna com o ALIAS actor e o título dos filmes. Os resultados devem ser ordenados pelos nomes de atores e atrizes. Use a imagem a seguir como referência.
+--Crie uma view chamada film_info utilizando as tabelas actor, film_actor e film do banco de dados sakila. Sua view deve exibir o actor_id, o nome completo do ator ou da atriz em uma coluna com o ALIAS actor e o título dos filmes. Os resultados devem ser ordenados pelos nomes de atores e atrizes. Use a imagem a seguir como referência.
+USE sakila;
+CREATE VIEW film_info AS (
+  SELECT
+  a.actor_id,
+  CONCAT(a.first_name, ' ', a.last_name) as `actor`,
+  f.title
+  FROM sakila.film AS f
+  JOIN sakila.film_actor AS fa ON f.film_id = fa.film_id
+  JOIN sakila.actor AS a ON fa.actor_id = a.actor_id
+  ORDER BY 2
+);
 
 --Resultado de exemplo para uma consulta na view do exercício 2
---Crie uma view chamada address_info que faça uso das tabelas address e city do banco de dados sakila . Sua view deve exibir o address_id , o address , o district , o city_id e a city . Os resultados devem ser ordenados pelo nome das cidades. Use a imagem abaixo como referência.
+--Crie uma view chamada address_info que faça uso das tabelas address e city do banco de dados sakila . Sua view deve exibir o address_id, o address, o district, o city_id e a city. Os resultados devem ser ordenados pelo nome das cidades. Use a imagem abaixo como referência.
 
 --Resultado de exemplo para uma consulta na view do exercício 3
 --Crie uma view chamada movies_languages , usando as tabelas film e language do banco de dados sakila . Sua view deve exibir o título do filme , o id do idioma e o idioma do filme , como na imagem a seguir.

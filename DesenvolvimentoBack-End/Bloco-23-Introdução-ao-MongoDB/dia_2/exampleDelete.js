7d650ddd1f59ba2aa758209a9cba7f32fcaccb10
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-06 16:11:55 -0300

    feat: delete operator example exercises

---
+++ DesenvolvimentoBack-End/Bloco-23-Introdução-ao-MongoDB/dia_2/exampleDelete.js
@@ -0,0 +1,5 @@
+// Remova o primeiro restaurante que possua culinária do tipo Ice Cream, Gelato, Yogurt, Ices.
+db.restaurants.deleteOne({ cuisine: "Ice Cream, Gelato, Yogurt, Ices" });
+
+// Remova todos os restaurantes que possuem culinária do tipo American .
+db.restaurants.deleteOne({ cuisine: "Ice Cream, Gelato, Yogurt, Ices" });

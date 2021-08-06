0178b6594e9f43e2f96eca28a3c1728940ddab49
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-06 15:50:42 -0300

    feat: example exercises 2 and 3

--- DesenvolvimentoBack-End/Bloco-23-Introdução-ao-MongoDB/dia_2/exampleExercises.js
+++ DesenvolvimentoBack-End/Bloco-23-Introdução-ao-MongoDB/dia_2/exampleExercises.js
@@ -1,3 +1,9 @@
 /*1.Selecione e faça a contagem dos restaurantes presentes nos bairros
 Queens , Staten Island e Bronx . (utilizando o atributo borough )*/
 db.restaurants.find({ 'borough': { $in: ['Bronx', 'Staten Island', 'Queens']} }).count();
+
+// 2. Selecione e faça a contagem dos restaurantes que não possuem culinária do tipo American . (utilizando o atributo cuisine )
+db.restaurants.find({cuisine:{ $ne: 'American' }}).count();
+
+//3. Selecione e faça a contagem dos restaurantes que possuem avaliação maior ou igual a 8 . (utilizando o atributo rating )
+db.restaurants.find({ rating :{ $gte: 8 }}).count();

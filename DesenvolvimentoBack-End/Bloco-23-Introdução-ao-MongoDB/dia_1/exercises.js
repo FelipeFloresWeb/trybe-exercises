4f00baf43e4d7af06eff0814beccc4ddec8dd2c9
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-06 15:18:23 -0300

    feat: exercises

--- DesenvolvimentoBack-End/Bloco-23-Introdução-ao-MongoDB/dia_1/exercises.js
+++ DesenvolvimentoBack-End/Bloco-23-Introdução-ao-MongoDB/dia_1/exercises.js
@@ -20,9 +20,13 @@ db.bios.find().skip(5).limit(2);
 
 
 // Exercício 7 : Retorne a quantidade de documentos da coleção books .
+db.books.count();
 
 // Exercício 8 : Conte quantos livros existem com o status = "PUBLISH" .
+db.books.count({ status: "PUBLISH" });
 
 // Exercício 9 : Exiba os atributos title , isbn e pageCount dos 3 primeiros livros. NÃO retorne o atributo _id .
+db.books.find( {}, {_id: 0, title: 1, isbn: 1, pageCount: 1 }).limit(3).pretty();
 
 // Exercício 10: Pule 5 documentos e exiba os atributos _id , title , authors e status dos livros com o status = "MEAP" , limitando-se a 10 documentos.
+db.books.find( { status: "MEAP" }, { title: 1, authors: 1, status: 1 }).skip(5).limit(10).pretty();

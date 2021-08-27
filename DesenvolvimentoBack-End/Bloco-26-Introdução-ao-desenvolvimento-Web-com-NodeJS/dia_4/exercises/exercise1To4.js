57ce9d13260b5ba216c2140122520cefc88b2b91
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-27 18:55:14 -0300

    feat to exercise 4

--- DesenvolvimentoBack-End/Bloco-26-Introdução-ao-desenvolvimento-Web-com-NodeJS/dia_4/exercises/exercise1To4.js
+++ DesenvolvimentoBack-End/Bloco-26-Introdução-ao-desenvolvimento-Web-com-NodeJS/dia_4/exercises/exercise1To4.js
@@ -8,6 +8,27 @@ app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (parseInt(age, 10) <= 17) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.body;
+
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });

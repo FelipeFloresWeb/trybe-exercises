d18182df7662e42575143e7d244b4d5baf3680f2
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-27 16:05:11 -0300

    added body examples

--- DesenvolvimentoBack-End/Bloco-26-Introdução-ao-desenvolvimento-Web-com-NodeJS/dia_4/hello-express/index.js
+++ DesenvolvimentoBack-End/Bloco-26-Introdução-ao-desenvolvimento-Web-com-NodeJS/dia_4/hello-express/index.js
@@ -1,15 +1,26 @@
 /* index.js */
+const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
-  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+  {
+    id: 1, name: 'Lasanha', price: 40.0, waitTime: 30,
+  },
+  {
+    id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25,
+  },
+  {
+    id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25,
+  },
 ];
 
-app.get('/recipes', function (req, res) {
-  res.json(recipes);
+app.post('/recipes', (req, res) => {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price });
+  res.status(201).json({ message: 'Recipe created successfully!' });
 });
 
 app.listen(3001, () => {

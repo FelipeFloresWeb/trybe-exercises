280374767f35999018f95364e1c083a445496656
Felipe Flores <felipefloresvendas@gmail.com>
2021-09-03 17:46:30 -0300

    added examples in png format code

--- DesenvolvimentoBack-End/Bloco-27-NodeJS-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia_2/ModelExampleMongoDb/index.js
+++ DesenvolvimentoBack-End/Bloco-27-NodeJS-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia_2/ModelExampleMongoDb/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const Author = require('./services/Author');
+const Author = require('./controllers/Author');
 
 app.use(bodyParser.json());
 

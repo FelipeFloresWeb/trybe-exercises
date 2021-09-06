aff663f7a6ed3ebc17801e69b4508c54c71fb5e3
Felipe Flores <felipefloresvendas@gmail.com>
2021-09-06 14:22:58 -0300

    added connection and gitignore archive

--- DesenvolvimentoBack-End/Bloco-27-NodeJS-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia_3/exercises/exerciseOne/models/connection.js
+++ DesenvolvimentoBack-End/Bloco-27-NodeJS-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia_3/exercises/exerciseOne/models/connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: 'localhost', // Se necessário, substitua pelo seu host, `localhost` é o comum
-  user: 'root', // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
-  password: 'senha123', // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
+  user: 'trybe', // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
+  password: 'trybe12345', // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
   database: 'rest_exercicios',
 });
 

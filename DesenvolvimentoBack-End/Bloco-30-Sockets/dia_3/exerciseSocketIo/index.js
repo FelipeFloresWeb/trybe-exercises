7f58f88cdf116f60f5885829ec3fa4453388b926
Felipe Flores <felipefloresvendas@gmail.com>
2021-10-20 09:47:54 -0300

    refatored app to REST patern

--- DesenvolvimentoBack-End/Bloco-30-Sockets/dia_3/exerciseSocketIo/index.js
+++ DesenvolvimentoBack-End/Bloco-30-Sockets/dia_3/exerciseSocketIo/index.js
@@ -10,10 +10,9 @@ const io = require('socket.io')(http, {
 
 io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
+  socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)');
 
-  socket.on('ping', () => {
-    console.log(`${socket.id} emitiu um ping!`);
-  });
+  require('./sockets/ping')(io);
 
 });
 

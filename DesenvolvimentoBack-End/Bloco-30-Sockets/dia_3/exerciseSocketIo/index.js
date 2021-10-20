8e0439e90ceb96dd7cfb4696152910511387c82a
Felipe Flores <felipefloresvendas@gmail.com>
2021-10-20 09:22:06 -0300

    added script to html socket.io page, and ping server

--- DesenvolvimentoBack-End/Bloco-30-Sockets/dia_3/exerciseSocketIo/index.js
+++ DesenvolvimentoBack-End/Bloco-30-Sockets/dia_3/exerciseSocketIo/index.js
@@ -12,6 +12,10 @@ io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
 });
 
+socket.on('ping', () => {
+  console.log(`${socket.id} emitiu um ping!`);
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
